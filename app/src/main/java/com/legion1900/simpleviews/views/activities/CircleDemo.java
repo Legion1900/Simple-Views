07d7e1a7bdbcb9924c440bc9b35d1b31d9ca65e7
Chumachenko Roman <rom4ik730@gmail.com>
2019-10-21 13:30:00 +0300

    Additional setup is done.

---
+++ app/src/main/java/com/legion1900/simpleviews/views/activities/CircleDemo.java
@@ -0,0 +1,16 @@
+package com.legion1900.simpleviews.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.legion1900.simpleviews.R;
+
+public class CircleDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_demo);
+    }
+}
--- app/src/main/java/com/legion1900/simpleviews/views/activities/MainActivity.java
+++ app/src/main/java/com/legion1900/simpleviews/views/activities/MainActivity.java
@@ -2,15 +2,25 @@ package com.legion1900.simpleviews.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.legion1900.simpleviews.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Intent viewDemo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewDemo = new Intent(this, CircleDemo.class);
+    }
+
+    public void onCircleDemoClick(View view) {
+        startActivity(viewDemo);
     }
 }
