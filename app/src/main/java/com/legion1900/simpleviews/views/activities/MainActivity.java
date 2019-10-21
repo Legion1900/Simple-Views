383df8354e4f6de2e0e2023a1293d633c9768c0f
Chumachenko Roman <rom4ik730@gmail.com>
2019-10-21 12:25:56 +0300

    Added packages and base code for CustomCircleView.

--- app/src/main/java/com/legion1900/simpleviews/views/activities/MainActivity.java
+++ app/src/main/java/com/legion1900/simpleviews/views/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.legion1900.simpleviews;
+package com.legion1900.simpleviews.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.legion1900.simpleviews.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/legion1900/simpleviews/views/customviews/CustomCircleView.java
@@ -0,0 +1,13 @@
+package com.legion1900.simpleviews.views.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomCircleView extends View {
+    public CustomCircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
